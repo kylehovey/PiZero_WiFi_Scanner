1d43c649a2f254667f7f4a5512760cccbecf93dd
kylehovey <khoveyc@gmail.com>
2017-06-14 08:50:17 -0700

    Updated look of results.

--- public/js/scanner/app.js
+++ public/js/scanner/app.js
@@ -14,11 +14,20 @@ class App {
    * @param {Object} network Network object
    */
   netToString(network) {
+    // Create the output
     var out = "";
+
+    // Classes for different keys
+    var style = {
+      ssid : "highlight",
+      mac : "highlight uppercase",
+      channel : "highlight",
+      rssi : "highlight"
+    };
 
     // Populate it with keys and vals of network
     $.each(network, (key, val) => {
-      out += `${key} : ${val}, <br />`;
+      out += `${key} : <span class="${style[key]}">${val}</span>, <br />`;
     });
 
     // Return the string without the trailing comma
